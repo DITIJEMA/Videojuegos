498e016436f4336eefcd03e82a5b0cad4af477af
DESTROLLER616 <criatopher2209@gmail.com>
2022-10-04 03:13:58 -0500

    Agregar la parte de las vostas de las paginas

---
+++ resources/views/videojuegos.blade.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="/resources//views/videojuegosPS.blade.php">// estilo
+
+   <!-- custom js file link  -->
+   <script src="/resources//views//videojuegos.js" defer></script>// scrip
+
+</head>
+<body>
+   
+<div class="container">
+
+
+   <h3 class="title">VIDEOJUEGOS</h3>
+
+   <div class="products-container">
+
+      <div class="product" data-name="p-1">
+         <img src="" alt="">
+         <h3></h3>
+         <div class="price"></div>
+      </div>
+
+      <div class="product" data-name="p-2">
+         <img src="" alt="">
+         <h3></h3>
+         <div class="price"></div>
+      </div>
+
+      <div class="product" data-name="p-3">
+         <img src="" alt="">
+         <h3></h3>
+         <div class="price"></div>
+      </div>
+
+      <div class="product" data-name="p-4">
+         <img src="" alt="">
+         <h3></h3>
+         <div class="price"></div>
+      </div>
+
+      <div class="product" data-name="p-5">
+         <img src="" alt="">
+         <h3></h3>
+         <div class="price"></div>
+      </div>
+
+      <div class="product" data-name="p-6">
+         <img src="" alt="">
+         <h3></h3>
+         <div class="price"></div>
+      </div>
+
+   </div>
+
+</div>
+
+<div class="products-preview">
+
+   <div class="preview" data-target="p-1">
+    <i class="fas fa-times"></i>
+    <img src="" alt="">
+    <h3></h3>
+    <p></p>
+    <div class="price"></div>
+    <div class="buttons">
+       <a href="#" class="buy">COMPRAR</a>
+       <a href="#" class="cart">AGREGAR</a>
+    </div>
+ </div>
+
+ <div class="preview" data-target="p-2">
+    <i class="fas fa-times"></i>
+    <img src="" alt="">
+    <h3></h3>
+    <p></p>
+    <div class="price"></div>
+    <div class="buttons">
+       <a href="#" class="buy">COMPRAR</a>
+       <a href="#" class="cart">AGREGAR</a>
+    </div>
+ </div>
+
+ <div class="preview" data-target="p-3">
+    <i class="fas fa-times"></i>
+    <img src="" alt="">
+    <h3></h3>
+    <p></p>
+    <div class="price"></div>
+    <div class="buttons">
+       <a href="#" class="buy">COMPRAR</a>
+       <a href="#" class="cart">AGREGAR</a>
+    </div>
+ </div>
+
+ <div class="preview" data-target="p-4">
+    <i class="fas fa-times"></i>
+    <img src="" alt="">
+    <h3></h3>
+    <p></p>
+    <div class="price"></div>
+    <div class="buttons">
+       <a href="#" class="buy">COMPRAR</a>
+       <a href="#" class="cart">AGREGAR</a>
+    </div>
+ </div>
+
+ <div class="preview" data-target="p-5">
+    <i class="fas fa-times"></i>
+    <img src="" alt="">
+    <h3></h3>
+    <p></p>
+    <div class="price"></div>
+    <div class="buttons">
+       <a href="#" class="buy">COMPRAR</a>
+       <a href="#" class="cart">AGREGAR</a>
+    </div>
+ </div>
+
+ <div class="preview" data-target="p-6">
+    <i class="fas fa-times"></i>
+    <img src="" alt="">
+    <h3></h3>
+    <p></p>
+    <div class="price"></div>
+    <div class="buttons">
+       <a href="#" class="buy">COMPRAR</a>
+       <a href="#" class="cart">AGREGAR</a>
+    </div>
+ </div>
+
+  
+
+</div>
+
+</body>
+</html> 
