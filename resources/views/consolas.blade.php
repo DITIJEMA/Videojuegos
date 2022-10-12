3fc33223819faefb9997818cf0db8deabdf1220f
DITIJEMA <mauaguila9@gmail.com>
2022-10-12 14:45:17 -0500

    algo

--- resources/views/consolas.blade.php
+++ resources/views/consolas.blade.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Consolas</title>
     </head>
-    <body>
+    <body>   
         <style>
 @import url('https://fonts.googleapis.com/css?family=Open+Sans|Roboto');
 
